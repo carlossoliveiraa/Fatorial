c4008bb9646a22d3a46541ec95088a14aa1b17ab
unknown <carlossoliveiraa@hotmail.com>
2023-09-21 10:21:26 -0300

    calculando o fatorial de um numero

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int numero = 5; 
+        long resultado = CalcularFatorial(numero);
+
+        Console.WriteLine($"O fatorial de {numero} é: {resultado}");
+    }
+
+    static long CalcularFatorial(int n)
+    {
+        if (n == 0)
+            return 1;
+        else
+            return n * CalcularFatorial(n - 1);
+    }
+}
